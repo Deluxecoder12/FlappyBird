5ad69277f2a6056e5ec6a4ed0757093b30a95e5d
Sakar Joshi <joshis@bgsu.edu>
2023-10-30 13:12:46 -0400

    Fixed collision and skins

--- flappyBird.py
+++ flappyBird.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((1000, 720))
 pygame.display.set_caption("Flappity")
 
 # Icon for title bar
-icon = pygame.image.load("flappyB.png")
+icon = pygame.image.load("logo.png").convert_alpha()
 pygame.display.set_icon(icon)
 
 # To control fps
@@ -20,51 +20,63 @@ clock = pygame.time.Clock()
 
 running = True
 x, y = 200, 360
-pipe_up_x, pipe_up_y = 600, -900
-pipe_down_x, pipe_down_y = 600, 300
+pipe_up_x, pipe_up_y = 900, -50
+pipe_down_x, pipe_down_y = 900, 550
 
-#sky_bg = pygame.image.load(".png")   To load regular surface as images
+sky_bg = pygame.image.load("background.jpg").convert_alpha()  #To load regular surface as images
 #text_surface = test_font.render(text, anti-alias = True -> smooth edges. for pixel art = False , color)
 
-top_pipe = pygame.Surface((200, 700))
-top_pipe.fill("green")
+top_pipe = pygame.transform.rotate(pygame.image.load("pipe.png").convert_alpha(), 180)
+top_pipe_rect = top_pipe.get_rect(topleft = (pipe_up_x, pipe_up_y))
 
-bottom_pipe = pygame.Surface((200, 700))
-bottom_pipe.fill("green")
+bottom_pipe = pygame.image.load("pipe.png").convert_alpha()
+bottom_pipe_rect = bottom_pipe.get_rect(topleft = (pipe_down_x, pipe_down_y))
 
 while running:
+    # Bird
+    bird_surf = pygame.image.load("bird.png").convert_alpha()
+    bird_rect = bird_surf.get_rect(center = (x, y))
+
+    rotated_image = pygame.transform.rotate(bird_surf, 45)
+    rotated_image_rect = rotated_image.get_rect(center=bird_rect.center)
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                if y >= 60:
-                    y -= 70
+                y -= 65
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
+    # fill the screen with a color to hide anything from last frame
     screen.fill("sky blue")
  
     # RENDER YOUR GAME HERE
 
-    # Bird
-    bird = pygame.draw.circle(screen, "yellow", (x, y), 30, 0)
-
+    #if pipe goes too left, render it back to the right as the obstacle
+    if top_pipe_rect.x < -200: top_pipe_rect.x = 900
+    if bottom_pipe_rect.x < -200: bottom_pipe_rect.x = 900
+    
     # Bird tries going across top and bottom border
-    if 10 <=  bird.y <= 650:
+    if 10 <=  bird_rect.y <= 650:
         y += 3
     else:
         running = False
 
-    screen.blit(top_pipe, (100 ,-600))
-    screen.blit(bottom_pipe, (50, 600))
+    screen.blit(sky_bg, (0, 0))
+    screen.blit(bird_surf, bird_rect)
+    screen.blit(top_pipe, top_pipe_rect)
+    screen.blit(bottom_pipe, bottom_pipe_rect)
 
-    # top_pipe = pygame.draw.rect(screen, "green", (pipe_up_x, pipe_up_y, 200, 1000))
-    # bottom_pipe = pygame.draw.rect(screen, "green", (pipe_down_x, pipe_down_y, 200, 1000))
+    if bird_rect.colliderect(top_pipe_rect) or bird_rect.colliderect(bottom_pipe_rect):
+        running = False
+    
+    top_pipe_rect.x -= 3
+    bottom_pipe_rect.x -= 3
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pygame.display.update()
 
     clock.tick(60)  # limits FPS to 60
 
