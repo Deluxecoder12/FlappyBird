bc51c1464b7d4200cfe17ca154b1f0a6a8439c51
Sakar Joshi <joshis@bgsu.edu>
2023-12-26 18:21:18 -0500

    Final Edits

--- flappyBird.py
+++ flappyBird.py
@@ -70,27 +70,6 @@ class Obstacle(pygame.sprite.Sprite):
         self.rect.x -= self.speed
         self.destroy()
 
-# Obstacle list
-def obstacle_gen(rect_list):
-    global score
-    global speed
-    if rect_list:
-        for i, j in rect_list:
-            screen.blit(top_pipe, j)
-            screen.blit(bottom_pipe, i)
-            j.x -= speed
-            i.x -= speed
-            if (i.x < -194):
-                score += 1
-        return [[top, bottom] for top, bottom in rect_list if top.x > -195] # deleting pipes that go off screen
-    return []
-
-def collisions(bird, obstacles):
-    if obstacles:
-        for obstacle1, obstacle2 in obstacles:
-            if bird.colliderect(obstacle1) or bird.colliderect(obstacle2): return False
-    return True
-
 def collision_sprite():
     if pygame.sprite.spritecollide(bird.sprite, obstacle_group, False):
         obstacle_group.empty()
@@ -121,21 +100,12 @@ speed = 3.0
 speed_gen = temp = 3500
 
 running = True
-x, y = 200, 360
 
 # fill the screen with a color to hide anything from last frame
 sky_bg = pygame.image.load("background.jpg").convert_alpha()  #To load regular surface as images
 #text_surface = test_font.render(text, anti-alias = True -> smooth edges. for pixel art = False , color)
 
-top_pipe = pygame.transform.rotate(pygame.image.load("pipe.png").convert_alpha(), 180)
-bottom_pipe = pygame.image.load("pipe.png").convert_alpha()
-
-# Bird
-bird_surf = pygame.image.load("up_bird.png").convert_alpha()
-bird_rect = bird_surf.get_rect(center = (x, y))
-
 # Timer
-obstacle_rect_list = []
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, speed_gen)
 
@@ -144,23 +114,18 @@ bird.add(Bird())
 
 obstacle_group = pygame.sprite.Group()
 
-while running:
-    # pipe_down_x, pipe_down_y = 1000, random.randint(300, 600)
-    # pipe_up_x, pipe_up_y = 1000, pipe_down_y - 600
+bg_music = pygame.mixer.Sound('FlappyBirdBG.mp3')
+bg_music.play(loops=-1)
 
-    # top_pipe_rect = top_pipe.get_rect(topleft = (pipe_up_x, pipe_up_y))
-    # bottom_pipe_rect = bottom_pipe.get_rect(topleft = (pipe_down_x, pipe_down_y))
-    
-    # For Score and font 
-    test_font = pygame.font.Font('Hack-Bold.ttf', 40)
+# For font
+test_font = pygame.font.Font('Hack-Bold.ttf', 40)
+
+while running:    
+    # For Score 
     text_surface = test_font.render('Score: ' + str(int(score)), False, 'Black')
 
     # poll for events
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                bird_gravity = -10
-
         if event.type == obstacle_timer:
             temp -= 100
 
@@ -180,28 +145,14 @@ while running:
         speed += 0.0000000000000000001
         temp = speed_gen
 
-    # # Bird tries going across top and bottom border
-    # temp_bird_y = bird_rect.y
-    # bird_gravity += 1
-    # if 70 <=  bird_rect.bottom <= 650: #and collisions(bird_rect, obstacle_rect_list):
-    #     bird_rect.y += bird_gravity
-    # # else:
-    # #     # Bird crashes to the borders or the pipes
-    # #     running = False
-    
-    # # Tilt bird when it falls
-    # if bird_rect.y - 10 > temp_bird_y:
-    #     bird_surf = pygame.image.load("down_bird.png").convert_alpha()
-    # else:
-    #     bird_surf = pygame.image.load("up_bird.png").convert_alpha()
-
     screen.blit(sky_bg, (0, 0))
+    
     obstacle_group.draw(screen)
     obstacle_group.update()
     
-    #screen.blit(bird_surf, bird_rect)
     bird.draw(screen)
     bird.update()
+    
     screen.blit(text_surface, (700, 30))
 
     running = collision_sprite()
